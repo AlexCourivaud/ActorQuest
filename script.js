e8b323c305f9964adc7899582f7d4527de915768
AlexCourivaud <alexandre.courivaud@gmail.com>
2024-09-13 17:26:29 +0200

    Working on js - display actors and actor description

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ import { api } from "./scripthide.js";
 
 //************** Variables *********************//
 const searchBar = document.querySelector(".search-bar");
-const actorList = document.querySelector(".actor-result");
+const actorsList = document.querySelector(".actor-result");
 const actorCard = document.querySelector(".actor-card");
 const searchInput = document.getElementById("search-input");
+// const actorResultList = document.querySelector(".actors");
 
 // // let searchConsole = searchInput.innerText;
 // // console.log(searchConsole);
@@ -15,53 +16,58 @@ function fctInputResearch() {
   // ***** on attrape la valeur entrée par l'utilisateur :
   searchInput.addEventListener("input", () => {
     var inputresearch = searchInput.value; // on la stock dans la variable
-    // fctDisplayActorsList(inputresearch); // on appelle la fct display avec le parametre de l'input
-    // console.log(inputresearch);
-    // return inputresearch;
-
+    // On récupère la BDD de l'api TMDB
     fetch(
-      `https://api.themoviedb.org/3/search/person?api_key=${api}&query=${inputresearch}&language=fr&page=1`
+      `https://api.themoviedb.org/3/search/person?api_key=${api}&query="${inputresearch}"&language=fr&page=1`
     )
       .then((response) => response.json())
       .then((data) => {
-        fctApiActors(data);
+        fctDisplayActorsList(data);
         // console.log(data);
       })
-      .then((response) => console.log(response))
+      .then((response) => console.log(response + " //////fin"))
       .catch((err) => console.error(err));
   });
 }
-
-// //TODO fetch de data from the movie DB
-
 // ********** Fct display ****/
+function fctDisplayActorsList(actorsAPI) {
+  let actorsName = actorsAPI.results;
+  //   console.log(actorsName);
+  actorsList.innerHTML = ""; // on vide le résultat avant de recommencer.
+  for (let i = 0; i < 20; i++) {
+    //
+    let actorsDisplay = document.createElement("div"); // on créer la balise
+    actorsDisplay.textContent = actorsName[i].name; //on ajoute le nom de l'acteur dans la var display
+    actorsDisplay.classList.add("actors"); // on ajoute une class aux acteurs.
+    if (actorsName[i].profile_path == null) {
+      // Fct if si l'acteur n'a pas de photo
+      var img = document.createElement("img"); // on crée l'élément img sur l'html
+      img.src = "assets/Unknown_person.jpg"; // on attrape l'asset photo inconnu
+      actorsList.appendChild(img); // on ajoute l'image à l'html
+      actorsList.appendChild(actorsDisplay); // on ajoute le nom de l'acteur
+      actorsDisplay.appendChild(img); // on ajoute l'image à l'html
+    } else {
+      var img = document.createElement("img"); // on crée l'élément img sur l'html
+      img.src = "https://image.tmdb.org/t/p/w500/" + actorsName[i].profile_path;
+      actorsList.appendChild(actorsDisplay); // on ajoute le nom de l'acteur
+      actorsDisplay.appendChild(img); // on ajoute l'image à l'html
+    }
+    actorsDisplay.addEventListener("click", () => {
+      console.log(actorsName[i].name);
+      actorCard.innerHTML = "";
 
-// //************** Fetch API themovieDB */
+      //   On veut appliquer les informations de l'acteur dans la zone actors CARD a droite
+      let actornewCard = document.createElement("div");
+      actornewCard.textContent = actorsName[i].name;
+      var actornewImg = document.createElement("img"); // on crée l'élément img sur l'html
 
-function fctApiActors(actorsApi) {
-  let actors = actorsApi.results;
-  for (let a in actors) {
-    let actor = actors[a];
-    // console.log(actor.name);
-    fctDisplayActorsList(actor);
+      actornewImg.src =
+        "https://image.tmdb.org/t/p/w500/" + actorsName[i].profile_path;
+      actorCard.appendChild(actornewCard);
+      actorCard.appendChild(actornewImg);
+    });
   }
-}
-
-function fctDisplayActorsList(actorsName) {
-  console.log(actorsName);
-  actorList.innerHTML = ""; // on vide le résultat avant de recommencer.
-  let actorsDisplay = document.createElement("p"); // on créer la balise
-  //   actorsDisplay.className("actor");
-
-  //   for (let i = 0; i < actorsName.length; i++) {
-  actorsDisplay.textContent = actorsName.name; //on ajoute le nom de l'acteur dans la var display
-  console.log(actorsDisplay);
-  actorsDisplay.classList.add("actors"); // on ajoute une class aux acteurs.
-  actorList.appendChild(actorsDisplay);
-  //   }
 }
 fctInputResearch();
 
-//         actorsDisplay.textContent = actor; //on ajoute le nom de l'acteur dans la var display
-// actorsDisplay.classList.add("actors"); // on ajoute une class aux acteurs.
-// actorList.appendChild(actorsDisplay);
+
